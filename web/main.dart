7211cb9a9f323d7f957e310d7f15fdd1cd0c571c
Harry Stern <hcs@synonymbun.com>
2014-05-18 19:03:43 -0400

    inital commit

---
+++ web/main.dart
@@ -0,0 +1,102 @@
+import 'dart:html';
+
+class ImageLoader {
+  Map<String, ImageElement> images;
+  Function complete;
+
+  int total;
+
+  ImageLoader(Function onCompletion, Map<String,String> load) {
+    images = new Map<String, ImageElement>();
+    total = load.length;
+    complete = onCompletion;
+
+    for (String img in load.keys) {
+      // because of cors this doesn't work for arbitrary urls
+      // workaround: upload to imgur and then grab the image data from there
+      // add new_image.crossOrigin = "Anonymous" i think
+      ImageElement new_image = new ImageElement(src: load[img]);
+      new_image.onLoad.listen( (e) => loaded(img, new_image));
+    }
+  }
+  void loaded(String img, ImageElement ele) {
+    images[img] = ele;
+    if (images.keys.length == total) {
+      complete(this);
+    }
+  }
+}
+
+class Pixel {
+  int r;
+  int g;
+  int b;
+  int a;
+  Pixel(this.r, this.g, this.b, this.a);
+}
+
+List<List<Pixel>> parse_image_data(ImageData data) {
+  var pixels = new List<List<Pixel>>(data.height);
+
+  var d = data.data;
+
+  int stride = 4;
+  for (int i = 0; i < data.height; i++) {
+    pixels[i] = new List<Pixel>(data.width);
+    for (int j = 0; j < data.width; j++) {
+      int index = (i*(data.width)*stride) + (j*stride);
+      Pixel p = new Pixel(d[index], d[index+1], d[index+2], d[index+3]);
+
+      pixels[i][j] = p;
+    }
+  }
+  return pixels;
+}
+
+ImageData unparse_image_data(List<List<Pixel>> pixels) {
+  int width = pixels.first.length;
+  int height = pixels.length;
+
+  var new_data = context.createImageData(width, height);
+  int stride = 4;
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      int index = (i*(width)*stride) + (j*stride);
+      new_data.data[index] = pixels[i][j].r;
+      new_data.data[index+1] = pixels[i][j].g;
+      new_data.data[index+2] = pixels[i][j].b;
+      new_data.data[index+3] = pixels[i][j].a;
+    }
+  }
+  return new_data;
+}
+
+void transform_pixels(List<List<Pixel>> pixels) {
+  int width = pixels.first.length;
+  int height = pixels.length;
+}
+
+CanvasElement canvas;
+CanvasRenderingContext2D context;
+
+void main() {
+  canvas = querySelector("#area");
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+
+  context = canvas.context2D;
+
+  ImageLoader loader = new ImageLoader(next, {'pepsi':"Pepsi_logo_2008.svg"});
+}
+
+void next(ImageLoader loader) {
+  var pepsi = loader.images['pepsi'];
+  context.drawImage(pepsi, 0,0);
+  var pixels = parse_image_data(context.getImageData(0,0,pepsi.width,pepsi.height));
+
+  transform_pixels(pixels);
+
+  context.putImageData(unparse_image_data(pixels), pepsi.width+20, 0);
+
+}
