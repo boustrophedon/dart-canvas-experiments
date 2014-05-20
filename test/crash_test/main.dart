5a2d8b7839599485a4347c6518c36474d0ed87e8
Harry Stern <hcs@synonymbun.com>
2014-05-20 01:30:35 -0400

    confirmed that writing to the canvas with the pixel data causes the crash

--- test/crash_test/main.dart
+++ test/crash_test/main.dart
@@ -1,11 +1,15 @@
 import 'dart:html';
 
+import 'dart:math' show Random;
+
 CanvasElement canvas;
 CanvasRenderingContext2D context;
+Random rng;
 
 void main() {
   canvas = querySelector("#area");
   context = canvas.context2D;
+  rng = new Random();
 
   run();
 }
@@ -18,6 +22,10 @@ void render(num time) {
   context.clearRect(0,0,canvas.height,canvas.width);
 
   ImageData pixels = context.getImageData(0,0,canvas.height,canvas.width);
+
+  for (int i = 0; i < pixels.data.length; i++) {
+    pixels.data[i] = rng.nextInt(255);
+  }
 
   context.putImageData(pixels, 0, 0);
 
