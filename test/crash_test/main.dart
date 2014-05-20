f44e64d298756c334466dd32b666c17b3fd5bfcd
Harry Stern <hcs@synonymbun.com>
2014-05-20 01:42:27 -0400

    removed random to simplify example

--- test/crash_test/main.dart
+++ test/crash_test/main.dart
@@ -1,15 +1,11 @@
 import 'dart:html';
 
-import 'dart:math' show Random;
-
 CanvasElement canvas;
 CanvasRenderingContext2D context;
-Random rng;
 
 void main() {
   canvas = querySelector("#area");
   context = canvas.context2D;
-  rng = new Random();
 
   run();
 }
@@ -24,7 +20,7 @@ void render(num time) {
   ImageData pixels = context.getImageData(0,0,canvas.height,canvas.width);
 
   for (int i = 0; i < pixels.data.length; i++) {
-    pixels.data[i] = rng.nextInt(255);
+    pixels.data[i] = 20;
   }
 
   context.putImageData(pixels, 0, 0);
